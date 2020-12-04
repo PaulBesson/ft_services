5037e2c79faaff2c1ec8fcf01bb35b0fe3a630dc
user42 <paul.besson@hotmail.com>
2020-12-04 16:18:17 +0100

    nginx redirection fixed

--- setup.sh
+++ setup.sh
@@ -1,20 +1,31 @@
+minikube start --driver=docker
+eval $(minikube docker-env)
+#-p minikube
+
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-minikube start --driver=docker
-eval $(minikube docker-env)
 docker build srcs/nginx -t service_nginx
 docker build srcs/wordpress -t service_wordpress
-docker build srcs/mysql -t mysql:5.6
+docker build srcs/mysql -t service_mysql
+docker build srcs/phpmyadmin -t service_phpmyadmin
 
-kubectl delete -f srcs/nginx.yaml
-kubectl delete -f srcs/wordpress-deployment.yaml
-kubectl delete -f srcs/mysql-deployment.yaml
+#kubectl create -f srcs/nginx/nginx.yaml --dry-run=client -o yaml | kubectl apply -f srcs/nginx/nginx.yaml
+#kubectl create -f srcs/wordpress/wordpress-deployment.yaml --dry-run=client -o yaml | kubectl apply -f srcs/wordpress/wordpress-deployment.yaml
+#kubectl create -f srcs/mysql/mysql-deployment.yaml --dry-run=client -o yaml | kubectl apply -f srcs/mysql/mysql-deployment.yaml
+#kubectl create -f srcs/metallb.yaml --dry-run=client -o yaml | kubectl apply -f srcs/metallb.yaml
+#kubectl create -f srcs/phpmyadmin/phpmyadmin-deployment.yaml --dry-run=client -o yaml | kubectl apply -f srcs/phpmyadmin/phpmyadmin-deployment.yaml
+
+kubectl delete -f srcs/nginx/nginx.yaml
+kubectl delete -f srcs/wordpress/wordpress-deployment.yaml
+kubectl delete -f srcs/mysql/mysql-deployment.yaml
+kubectl delete -f srcs/phpmyadmin/phpmyadmin-deployment.yaml
 kubectl delete -f srcs/metallb.yaml
 
-kubectl create -f srcs/nginx.yaml
-kubectl create -f srcs/wordpress-deployment.yaml
-kubectl create -f srcs/mysql-deployment.yaml
+kubectl create -f srcs/nginx/nginx.yaml
+kubectl create -f srcs/wordpress/wordpress-deployment.yaml
+kubectl create -f srcs/mysql/mysql-deployment.yaml
+kubectl create -f srcs/phpmyadmin/phpmyadmin-deployment.yaml
 kubectl create -f srcs/metallb.yaml
--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,15 +1,16 @@
 # setup mysql
 mysql_install_db --user=mysql --datadir=/var/lib/mysql
-mkdir /run/openrc
+mkdir /run/openrc # a virer (peut-etre) ######################################
 touch /run/openrc/softlevel
 #/etc/init.d/mariadb setup
+openrc
 rc-service mariadb start
 
 echo "CREATE DATABASE wordpress;" | mysql -u root # on cree la base de donnee
-echo "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';" | mysql -u root # on cree l'user
-echo "SET PASSWORD FOR root@localhost=PASSWORD('password');" | mysql -u root # le mot de passe
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'localhost';" | mysql -u root # le privileges (root)
-
+echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'password';" | mysql -u root # on cree l'user
+#echo "SET PASSWORD FOR %=PASSWORD('password');" | mysql -u root # le mot de passe
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'%';" | mysql -u root # le privileges (root)
+echo "FLUSH PRIVILEGES;" | mysql -u root
 rc-service mariadb restart
 
 while true; do
