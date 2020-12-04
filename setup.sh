b51bd523cbd8dbb38a861aa1c642f697a0be268c
user42 <paul.besson@hotmail.com>
2020-12-04 18:20:49 +0100

    vsftpd ongoing

--- setup.sh
+++ setup.sh
@@ -12,11 +12,11 @@ docker build srcs/wordpress -t service_wordpress
 docker build srcs/mysql -t service_mysql
 docker build srcs/phpmyadmin -t service_phpmyadmin
 
-#kubectl create -f srcs/nginx/nginx.yaml --dry-run=client -o yaml | kubectl apply -f srcs/nginx/nginx.yaml
-#kubectl create -f srcs/wordpress/wordpress-deployment.yaml --dry-run=client -o yaml | kubectl apply -f srcs/wordpress/wordpress-deployment.yaml
-#kubectl create -f srcs/mysql/mysql-deployment.yaml --dry-run=client -o yaml | kubectl apply -f srcs/mysql/mysql-deployment.yaml
-#kubectl create -f srcs/metallb.yaml --dry-run=client -o yaml | kubectl apply -f srcs/metallb.yaml
-#kubectl create -f srcs/phpmyadmin/phpmyadmin-deployment.yaml --dry-run=client -o yaml | kubectl apply -f srcs/phpmyadmin/phpmyadmin-deployment.yaml
+#kubectl apply -f srcs/nginx/nginx.yaml
+#kubectl apply -f srcs/wordpress/wordpress-deployment.yaml
+#kubectl apply -f srcs/mysql/mysql-deployment.yaml
+#kubectl apply -f srcs/metallb.yaml
+#kubectl apply -f srcs/phpmyadmin/phpmyadmin-deployment.yaml
 
 kubectl delete -f srcs/nginx/nginx.yaml
 kubectl delete -f srcs/wordpress/wordpress-deployment.yaml
