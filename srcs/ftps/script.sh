66397c95950b335dd822cc91ba3343e31f332da6
user42 <paul.besson@hotmail.com>
2021-01-06 19:30:04 +0100

    ftps : ok

--- srcs/ftps/script.sh
+++ srcs/ftps/script.sh
@@ -1,4 +1,8 @@
 
+RUN mkdir /home/admin/ftp && chown nobody:nogroup /home/admin/ftp
+RUN chmod a-w /home/admin/ftp/
+RUN mkdir /home/admin/ftp/files && chown admin:password /home/admin/ftp/files
+
 service vsftpd start
 
 while true; do
