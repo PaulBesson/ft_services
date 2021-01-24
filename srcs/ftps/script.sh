632b5b7cbb6d68266a7a4d80bdfd44843e830fff
user42 <paul.besson@hotmail.com>
2021-01-24 22:49:34 +0100

    influxdb fixed

--- srcs/ftps/script.sh
+++ srcs/ftps/script.sh
@@ -6,15 +6,13 @@ RUN mkdir /home/admin/ftp/files && chown admin:password /home/admin/ftp/files
 service vsftpd start
 
 #
-sudo mkdir /etc/ssl/private
+mkdir /etc/ssl/private
 openssl req -x509 -nodes -days 365 -newkey rsa:1024 -subj \
 	'/C=FR/ST=Auvergne-Rhone-Alpes/L=Lyon/emailAddress=pbesson@student.42lyon.fr' \
 	-keyout /etc/ssl-vsftpd/private/vsftpd.pem \
 	-out /etc/ssl-vsftpd/private/vsftpd.pem
-sudo ufw allow 990/tcp
-sudo ufw allow 40000:50000/tcp
-sudo ufw status
-$ systemctl restart vsftpd
+#$ systemctl restart vsftpd
+service vsftpd restart
 #
 
 while true; do
