52e668beb21baf30516d8b140a8ecaeb74dc033a
Paul Besson <pbesson@z2r8p8.42lyon.fr>
2021-05-12 16:06:38 +0200

    hopefully the end

--- srcs/ftps/script.sh
+++ srcs/ftps/script.sh
@@ -1,7 +1,6 @@
-mkdir /home/admin/ftp && chown nobody:nogroup /home/admin/ftp
-chmod a-w /home/admin/ftp
-
-mkdir /home/admin/ftp/files && chown admin:password /home/admin/ftp/files
+# mkdir /home/admin/ftp && chown nobody:nogroup /home/admin/ftp
+# chmod a-w /home/admin/ftp/
+# mkdir /home/admin/ftp/files && chown admin:password /home/admin/ftp/files
 
 ./telegraf/telegraf & rc-service vsftpd restart
 
