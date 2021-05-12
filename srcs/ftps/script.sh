56cf6dd0809670f5e82e0834a066624cfe0311e0
Paul Besson <pbesson@z2r8p8.42lyon.fr>
2021-05-12 15:07:57 +0200

    ftps can't transfer

--- srcs/ftps/script.sh
+++ srcs/ftps/script.sh
@@ -1,9 +1,10 @@
 mkdir /home/admin/ftp && chown nobody:nogroup /home/admin/ftp
-chmod a-w /home/admin/ftp/
+chmod a-w /home/admin/ftp
+
 mkdir /home/admin/ftp/files && chown admin:password /home/admin/ftp/files
 
 ./telegraf/telegraf & rc-service vsftpd restart
 
 while true; do
-    sleep 5;
+    sleep 1;
 done;
